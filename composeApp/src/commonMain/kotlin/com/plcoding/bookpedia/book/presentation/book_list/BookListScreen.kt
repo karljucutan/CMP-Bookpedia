7ff400643ef99bb82be1e8393eb2ae31e1e116a2
karljucutan <karljucutan@gmail.com>
2025-11-18 21:01:20 +0800

    Implement BookList and BookListItem components with search functionality

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,10 +1,17 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -23,14 +30,38 @@ fun BookListScreenRoot(
                 else -> Unit
             }
             viewModel.onAction(action)
-        }
+        },
+        modifier = modifier
     )
 }
 
 @Composable
-private fun BookListScreen(
+fun BookListScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    // Implementation of the BookListScreen composable function
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+            modifier = modifier
+                .widthIn(max = 400.dp)
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
 }
