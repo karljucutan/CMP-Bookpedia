a5af9ab06bf6c344b39d2dfa0533bf9733d61b2f
karljucutan <karljucutan@gmail.com>
2025-11-22 19:24:39 +0800

    Refactor BookListState to initialize searchResults as empty list and comment out sample data

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,7 +5,7 @@ import com.plcoding.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = books,
+    val searchResults: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
@@ -13,18 +13,18 @@ data class BookListState(
 )
 
 // Sample Data
-private val books = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book $it",
-        imageUrl = "https://test.com",
-        authors = listOf("Karl Jucutan"),
-        description = "Description $it",
-        languages = emptyList(),
-        firstPublishYear = null,
-        averageRating = 4.67854,
-        ratingCount = 5,
-        numPages = 100,
-        numEditions = 3
-    )
-}
+//private val books = (1..100).map {
+//    Book(
+//        id = it.toString(),
+//        title = "Book $it",
+//        imageUrl = "https://test.com",
+//        authors = listOf("Karl Jucutan"),
+//        description = "Description $it",
+//        languages = emptyList(),
+//        firstPublishYear = null,
+//        averageRating = 4.67854,
+//        ratingCount = 5,
+//        numPages = 100,
+//        numEditions = 3
+//    )
+//}
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -55,6 +55,7 @@ fun BookListItem(
                 val painter = rememberAsyncImagePainter(
                     model = book.imageUrl,
                     onSuccess = {
+                        imageLoadResult =
                         if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
                             Result.success(it.painter)
                         } else {
